97105aff30f33e08c8c40c632e11966dd4fcd5ed
Rodrigo Ortiz <95726702+roroaka2001@users.noreply.github.com>
2022-11-17 20:24:11 -0300

    Mod Procesamiento

--- procesamiento.py
+++ procesamiento.py
@@ -1,19 +1,23 @@
 import numpy as np
 from torch.utils.data import TensorDataset, DataLoader
 import torch
+import matplotlib.pyplot as plt
 
 def recorte(data,dim=21):
     muestras,ancho,largo,canales=data.shape
+    print(data.shape)
+    #len(data[0])
+    my_x = np.ndarray(shape=(muestras, 21, 21,3))
     # Recorte
-    extremo_izq=(ancho-dim)//2
-    extremo_der=extremo_izq+dim
+
     # nuevo dataset 
-    new_data=np.zeros([muestras,dim,dim,canales])
-    for i in range(muestras):
-        new_data[i]=data[i][extremo_izq:extremo_der,
-                                                extremo_izq:extremo_der]
+    new_data=np.zeros(shape=(muestras, 21, 21,3))
+    for k in range(muestras):
+        new_data[k] = data[k][ 20:41 , 20:41 ]
     # Redimension y a tensor
-    my_x = new_data.reshape(muestras, canales, dim, dim) 
-    tensor_x = torch.Tensor(my_x) 
+    # my_x = np.transpose(new_data , 0, 3, 1, 2) 
+    tensor_x = torch.Tensor(new_data)
+    print(tensor_x.shape)
+    tensor_x = tensor_x.permute(0, 3, 1, 2)
     my_datatrain = TensorDataset(tensor_x,tensor_x) 
-    return my_datatrain
+    return my_datatrain  
